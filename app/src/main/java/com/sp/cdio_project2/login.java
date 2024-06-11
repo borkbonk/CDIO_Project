60bdd9eb3558979be7561228297959ae703da5ec
Bork <lau_yh.22@ichat.sp.edu.sg>
2024-06-11 10:02:34 +0800

    First commit

---
+++ app/src/main/java/com/sp/cdio_project2/login.java
@@ -0,0 +1,59 @@
+package com.sp.cdio_project2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
+
+public class login extends AppCompatActivity {
+
+    EditText emailEditText,passwordEditText;
+    Button loginBtn;
+    ProgressBar progressBar;
+    TextView createAccountBtnTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+        View view =getSupportActionBar().getCustomView();
+
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        loginBtn = findViewById(R.id.login_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email =emailEditText.getText().toString();
+                String password =passwordEditText.getText().toString();
+
+                if (email.equals("Admin") && password.equals("123")) {
+                    // Successful login
+                    Toast.makeText(login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(login.this, bottom_nav.class);
+                    startActivity(intent);
+
+                } else {
+                    // Failed login
+                    Toast.makeText(login.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+}
